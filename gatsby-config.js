cf18074cbc9cac736c269ef205265884909897e5
Neerav Mehta <neeravbm@yahoo.com>
2020-12-14 15:29:28 -0800

    Adding Rudder tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,6 +123,59 @@ module.exports = {
         delay_timeout: 0
       }
     },
+    {
+      resolve: `gatsby-plugin-rudderstack`,
+      options: {
+        // your rudderstack write key for your production environment
+        // when process.env.NODE_ENV === 'production'
+        // required; non-empty string
+        //NOTE: Do not commit this to git. Process from env.
+        prodKey: process.env.RUDDERSTACK_PRODUCTION_WRITE_KEY,
+        devKey: process.env.RUDDERSTACK_PRODUCTION_WRITE_KEY,
+
+        // boolean (defaults to false) on whether you want
+        // to include analytics.page() automatically
+        // if false, see below on how to track pageviews manually
+        trackPage: true,
+
+        // number (defaults to 50); time to wait after a route update before it should
+        // track the page change, to implement this, make sure your `trackPage` property is set to `true`
+        trackPageDelay: 50,
+
+        // If you need to proxy events through a custom endpoint,
+        // add a `host` property (defaults to https://hosted.rudderlabs.com )
+        // Rudderstack docs:
+        //   - https://docs.rudderstack.com/rudderstack-sdk-integration-guides/rudderstack-javascript-sdk#2-1-minified-code
+        host: `https://rudderstack-dataplane.rudderstack.com`,
+
+        // boolean (defaults to false); whether to delay load Rudderstack
+        // ADVANCED FEATURE: only use if you leverage client-side routing (ie, Gatsby <Link>)
+        // This feature will force Rudderstack to load _after_ either a page routing change
+        // or user scroll, whichever comes first. This delay time is controlled by
+        // `delayLoadTime` setting. This feature is used to help improve your website's
+        // TTI (for SEO, UX, etc).  See links below for more info.
+        // NOTE: But if you are using server-side routing and enable this feature,
+        // Rudderstack will never load (because although client-side routing does not do
+        // a full page refresh, server-side routing does, thereby preventing Rudderstack
+        // from ever loading).
+        // See here for more context:
+        // GIF: https://github.com/benjaminhoffman/gatsby-plugin-segment/pull/19#issuecomment-559569483
+        // TTI: https://github.com/GoogleChrome/lighthouse/blob/master/docs/scoring.md#performance
+        // Problem/solution: https://marketingexamples.com/seo/performance
+        delayLoad: false,
+
+        // number (default to 1000); time to wait after scroll or route change
+        // To be used when `delayLoad` is set to `true`
+        delayLoadTime: 1000,
+
+        // Whether to completely skip calling `analytics.load()`.
+        // ADVANCED FEATURE: only use if you are calling `analytics.load()` manually
+        // elsewhere in your code or are using a library
+        // that will call it for you.
+        // Useful for only loading the tracking script once a user has opted in to being tracked, for example.
+        manualLoad: false
+      }
+    },
     { 
       resolve: `gatsby-plugin-purgecss`,
       options: {
--- src/components/blogPostSubscriptionForm.js
+++ src/components/blogPostSubscriptionForm.js
@@ -1,9 +1,71 @@
-import React from "react"
+import React, {useState} from "react"
 import styles from "../assets/css/blog-post-subscription-form.module.css"
 
 const BlogPostSubscriptionForm = (props) => {
   const onBlogListingPage = props.onBlogListingPage ? "onBlogListingPage" : ""
   const htmlId = props.htmlId
+
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(false);
+  const [formSubmittedSuccessfully, setFormSubmittedSuccessfully] = useState(false);
+
+  const formSubmitted = (e) => {
+    e.preventDefault();
+
+    setFormSubmittedSuccessfully(false);
+
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(email).toLowerCase())) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+
+    if (!window.rudderanalytics) {
+      return;
+    }
+
+    const params = new URLSearchParams(document.location.search.substring(1));
+
+    window.rudderanalytics.identify('', {
+        email: email,
+        form_id: "Blog-header-Subscribe-form",
+        utm_source: params.get('utm_source'),
+        utm_medium: params.get('utm_medium'),
+        utm_campaign: params.get('utm_campaign'),
+        utm_content: params.get('utm_content'),
+        utm_term: params.get('utm_term'),
+        raid: params.get('raid'),
+        test_user: params.get('test_user')
+      },
+      {
+        'integrations': {
+          'Salesforce': true
+        }
+      });
+
+    window.rudderanalytics.track('form_submit', {
+        page: document.title,
+        page_URL: window.location.href,
+        form_id: "Blog-header-Subscribe-form",
+        utm_source: params.get('utm_source'),
+        utm_medium: params.get('utm_medium'),
+        utm_campaign: params.get('utm_campaign'),
+        utm_content: params.get('utm_content'),
+        utm_term: params.get('utm_term'),
+        raid: params.get('raid'),
+        test_user: params.get('test_user')
+      },
+      {
+        traits: {
+          email: email,
+          form_id: "Blog-header-Subscribe-form",
+        }
+      });
+
+    setFormSubmittedSuccessfully(true);
+  }
 
   return (
     <div
@@ -24,21 +86,36 @@ const BlogPostSubscriptionForm = (props) => {
               <input
                 type="email"
                 name="email"
-                defaultValue
+                value={email}
                 size={40}
                 className={styles.subscriptionFormEmailInput}
                 id="email"
                 aria-required="true"
                 aria-invalid="false"
                 placeholder="you@company.com"
+                onChange={(e) => {
+                  setError(false);
+                  setEmail(e.target.value);
+                  setFormSubmittedSuccessfully(false);
+                }}
               />
             </span>
+            {error &&
+              <span className={styles.error} role="alert" aria-hidden="true">The email address entered is invalid.</span>
+            }
           </p>
           <p className={styles.subscriptionFormParaLast}>
-            <input type="submit" defaultValue="Subscribe" className={styles.subscriptionFormSubmit}/>
+            <input
+              type="submit"
+              defaultValue="Subscribe"
+              className={styles.subscriptionFormSubmit}
+              onClick={e => formSubmitted(e)}
+            />
             <span className="ajax-loader"/>
           </p>
-          <div className={styles.subscriptionFormOutput} role="alert" aria-hidden="true"/>
+          {formSubmittedSuccessfully &&
+          <div className={styles.subscriptionFormOutput} role="alert" aria-hidden="true">Thank you for subscribing to our blog.</div>
+          }
         </form>
       </div>
     </div>
